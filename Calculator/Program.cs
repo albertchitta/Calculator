456ac76ebe8393a0608c556fea95c96061b0d066
albertchitta <albert.w.chittaphong@gmail.com>
2022-01-12 21:46:25 -0600

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an operator followed by a series of numbers (Ex. * 1,2,3):");
+            string input = Console.ReadLine();
+            string[] op = input.Split(" ");
+            string[] numbers = op[1].Split(",");
+
+            int result;
+
+            if (op[0] == "*")
+            {
+                result = 1;
+            }
+            else if (op[0] == "/")
+            {
+
+            }
+            else if (op[0] == "+")
+            {
+
+            }
+            else if (op[0] == "-")
+            {
+
+            }
+
+            foreach (var number in numbers)
+            {
+                int intNumber = int.Parse(number);
+                if (op[0] == "*")
+                {
+                    result *= intNumber;
+                }
+                else if (op[0] == "/")
+                {
+                    result /= intNumber;
+                }
+                else if (op[0] == "+")
+                {
+                    result += intNumber;
+                }
+                else if (op[0] == "-")
+                {
+                    result -= intNumber;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
